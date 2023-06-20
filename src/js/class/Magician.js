c5db4ec48475797f783251be13653bd6471d2e47
NikitaKx9 <nikitakx9@gmail.com>
2023-06-20 16:29:37 +0300

    class Magican rework

--- src/js/class/Magician.js
+++ src/js/class/Magician.js
@@ -3,7 +3,23 @@ import Character from './Character';
 export default class Magician extends Character {
   constructor(name, type = 'Magician') {
     super(name, type);
-    this.attack = 10;
+    // this.attack = 10;
     this.defence = 40;
+
+    
   }
-}
+  get attack() {
+    return this._attack;
+  }
+
+  set attack(range) {
+    if (range) {
+      this._attack = 10;
+    }
+    if (range == 1) {
+      this._attack = 10;
+    }
+    this._attack = value;
+  }
+};
+
