39c107b498e81cbc73b1bb709d3120b71770ccc9
NikitaKx9 <nikitakx9@gmail.com>
2023-06-23 16:41:49 +0300

    Magician chenged

--- src/js/class/Magician.js
+++ src/js/class/Magician.js
@@ -3,43 +3,35 @@ import Character from './Character';
 export default class Magician extends Character {
   constructor(name, type = 'Magician') {
     super(name, type);
-    this.attack = 10;
+    this._attack = 100;
     this.defence = 40;
     
   }
-
   get attack() {
-    return this.attack;
+    return this._attack;
   }
 
   set attack (range) {
-    if (range == 1) {
-      this.attack = 10;
-    }
-    if (range == 2) {
-      this.attack = 10 - (10 * 0.1);
-    }
-    if (range == 3) {
-      this.attack = 10 - (10 * 0.2);
-    }
-    if (range == 4) {
-      this.attack = 10 - (10 * 0.3);
-    }
-    if (range == 5) {
-      this.attack = 10 - (10 * 0.4);
-    }
-    return this.attack;
+    
+    let step =  (range / 10 - 0.1).toFixed(1);
+
+    this._attack = this._attack - (this._attack * step);
+   
   }
 
   get stoned () {
-    return this.attack
+    return this._attack
   }
   set stoned (range) {
-    this.attack = this.attack - Math.log(range) * 5
-     return this.attack
+    let step =  (range / 10 - 0.1).toFixed(1);
+
+    this._attack = (this._attack - (this._attack * step)) - (Math.log2(range) * 5);
+
+    return this._attack
   }
+  
 };
  let mage = new Magician("sofia");
- mage.set(2)
+ mage.stoned = 2;
  console.log(mage.attack)
 
