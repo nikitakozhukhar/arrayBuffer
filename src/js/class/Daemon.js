35c91b613737f1789e14ddc405712934569356fa
NikitaKx9 <nikitakx9@gmail.com>
2023-07-14 15:34:30 +0300

    conflict resolved

--- src/js/class/Daemon.js
+++ src/js/class/Daemon.js
@@ -3,7 +3,24 @@ import Character from './Character';
 export default class Daemon extends Character {
   constructor(name, type = 'Daemon') {
     super(name, type);
-    this.attack = 10;
+    this._attack = 10;
     this.defence = 40;
+  }
+  get attack() {
+    return this._attack;
+  }
+
+  set attack(range) {
+    let step = (range / 10 - 0.1).toFixed(1);
+    this._attack = this._attack - (this._attack * step);
+  }
+
+  get stoned() {
+    return this._attack
+  }
+  set stoned(range) {
+    let step = (range / 10 - 0.1).toFixed(1);
+    this._attack = (this._attack - (this._attack * step)) - (Math.log2(range) * 5);
+    return this._attack
   }
 }
